0a95204b529e886eb2e8c46ea4f5f75902237134
SidXSid <siddhartha.pachhai@gmail.com>
2023-08-20 12:52:52 -0700

    Update coursera_advanced_mysql_week_1_lab_trigger.sql

--- coursera_advanced_mysql_week_1_lab_trigger.sql
+++ coursera_advanced_mysql_week_1_lab_trigger.sql
@@ -1,1 +1,68 @@
+DELIMITER //
 
+DROP TRIGGER if exists ProductSellPriceInsertCheck//
+
+
+CREATE TRIGGER ProductSellPriceInsertCheck
+BEFORE INSERT on Products FOR EACH ROW
+BEGIN
+ if NEW.SellPrice <= NEW.BuyPrice THEN
+  INSERT into Notifications VALUES(NULL,concat("ProductID",NEW.ProductID, "was updated with a SellPrice of",
+  NEW.SellPrice,"which is the same or less than the BuyPrice"),CURRENT_TIME());
+
+ END IF;
+
+END //
+
+
+DROP TRIGGER if exists ProductSellPriceUpdateCheck//
+
+
+CREATE TRIGGER ProductSellPriceUpdateCheck
+BEFORE UPDATE on Products FOR EACH ROW
+BEGIN
+ if NEW.SellPrice <= NEW.BuyPrice THEN
+  INSERT into Notifications VALUES(NULL,concat("ProductID",NEW.ProductID, "was updated with a SellPrice of",
+  NEW.SellPrice,"which is the same or less than the BuyPrice"),CURRENT_TIME());
+
+ END IF;
+
+END //
+
+
+DROP TRIGGER if exists NotifyProductDelete//
+
+
+CREATE TRIGGER NotifyProductDelete
+AFTER DELETE on Products FOR EACH ROW
+BEGIN
+  INSERT into Notifications VALUES(NULL,concat("The product with a ProductID",OLD.ProductID,"was deleted"),CURRENT_TIME());
+END //
+
+
+DELIMITER ;
+
+
+INSERT into Products VALUES("P7", "Product 7", 40, 40, 100);
+
+select * from Products;
+
+select * from Notifications;
+
+DELETE from Products where ProductID = "P7";
+
+
+INSERT into Products VALUES("P8", "Product 8", 40, 50, 100);
+
+select * from Products;
+
+select * from Notifications;
+
+
+UPDATE Products set SellPrice = 40 where ProductID = "P8";
+
+select * from Products;
+
+select * from Notifications;
+
+DELETE from Products where ProductID = "P8";
